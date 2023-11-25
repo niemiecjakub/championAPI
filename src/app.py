ac0cd11d74c318ebc5afecd401e24ce0c56c8938
niemiecjakub <kniemiec3@gmail.com>
2023-11-25 16:55:45 +0100

    added flask-cors support

--- src/app.py
+++ src/app.py
@@ -3,32 +3,41 @@ import config
 from flask import render_template
 from champions import champion_read_all, champion_read_by_key
 from category import category_read_all, category_read_by_key, category_get_list
+from flask_cors import CORS, cross_origin
 
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
+CORS(app.app) 
+app.app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route("/")
+@cross_origin()
 def home():
     category_list = category_get_list()
     return render_template("home.html", category_list=category_list)
 
 #CHAMPION
 @app.route("/champion")
+@cross_origin()
 def champions():
     champions = champion_read_all()
     return render_template("champions.html", champions=champions)
 
 @app.route("/champion/<key>")
+@cross_origin()
 def champion(key):
     champion = champion_read_by_key(key)
     return render_template("champion.html", champion=champion)
 
 @app.route("/<category>")
+@cross_origin()
 def cateogies(category):
     data = category_read_all(category)
     return render_template("categories.html", category=category, data=data )
 
 @app.route("/<category>/<name>")
+@cross_origin()
 def category(category, name):
     category = category_read_by_key(category, name)
     return render_template("category.html", category=category, name=name)
--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS, cross_origin
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
@@ -9,6 +10,8 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'champions.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
