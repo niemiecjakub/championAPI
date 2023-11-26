59c1a551dea0f0156cc53ea4da5817cbe9c773e2
niemiecjakub <kniemiec3@gmail.com>
2023-11-26 20:14:45 +0100

    added champion name list endpoint

--- src/champions.py
+++ src/champions.py
@@ -6,6 +6,14 @@ def champion_read_all():
     champions = Champion.query.all()
     return ChampionSchema(many=True).dump(champions)
 
+def champion_read_by_name(name):
+    champion = Champion.query.filter(Champion.name == name).one_or_none()
+
+    if champion is not None:
+        return ChampionSchema().dump(champion)
+    else:
+        abort(404, f"Champion with name {name} not found")  
+
 def champion_read_by_key(key):
     champion = Champion.query.filter(Champion.key == key).one_or_none()
 
@@ -22,3 +30,9 @@ def champion_read_by_id(id):
     else:
         abort(404, f"Champion with id {id} not found")    
 
+def champion_name_list():
+    champions = Champion.query.all()
+    names = []
+    for champion in champions:
+        names.append(champion.name)
+    return {"champions": names}
