6cf0684d67dd9fc2c42e8fbf22c4f75536912411
Jakub Niemiec <kniemiec3@gmail.com>
2023-11-29 16:27:00 +0100

    init websocket

--- src/app.py
+++ src/app.py
@@ -42,99 +42,16 @@ def category(category, name):
     return render_template("category.html", category=category, name=name)
 
 
-# ROOMS
-rooms = {}
-
-def generate_room_code(length) :
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
-        if code not in rooms:
-            break
-    return code
-
-
-@app.route("/roomlobby", methods = ["POST", "GET"])
-@cross_origin()
-def browse_rooms():
-    if request.method == "POST":
-        name = request.form.get("name")
-        code = request.form.get("code")
-        join = request.form.get("join", False)
-        create = request.form.get("create", False)
-
-        if not name:
-            return render_template("roomlobby.html", error="Please enter name")
-        if join != False and not code:
-            return render_template("roomlobby.html", error="Please enter room code")
-    
-        room = code
-        if create != False:
-            room = generate_room_code(4)
-            rooms[room] = {"members": 0, "messages": []}
-        elif code not in rooms:
-            return render_template("roomlobby.html", error="Room does not exist.", code=code, name=name)
-        
-        session["room"] = room
-        session["name"] = name
-        return redirect(url_for("room"))
-
-
-    return render_template("roomlobby.html")
-
-
-@app.route("/room")
-def room():
-    room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("roomlobby"))
-
-    return render_template("room.html", code=room, messages=rooms[room]["messages"])
-
-
-@config.socketio.on("message")
-def message(data):
-    room = session.get("room")
-    if room not in rooms:
-        return 
-    
-    content = {
-        "name": session.get("name"),
-        "message": data["data"]
-    }
-    send(content, to=room)
-    rooms[room]["messages"].append(content)
-    print(f"{session.get('name')} said: {data['data']}")
 
 @config.socketio.on("connect")
-def connect(auth):
-    room = session.get("room")
-    name = session.get("name")
-    if not room or not name:
-        return
-    if room not in rooms:
-        leave_room(room)
-        return
-    
-    join_room(room)
-    send({"name": name, "message": "has entered the room"}, to=room)
-    rooms[room]["members"] += 1
-    print(f"{name} joined room {room}")
+def handle_connected():
+    print("client has connected")
 
-@config.socketio.on("disconnect")
-def disconnect():
-    room = session.get("room")
-    name = session.get("name")
-    leave_room(room)
+@config.socketio.on("send_message")
+def send_message(data):
+    print(data)
+    emit("send_message", data, broadcast=True)
 
-    if room in rooms:
-        rooms[room]["members"] -= 1
-        if rooms[room]["members"] <= 0:
-            del rooms[room]
-    
-    send({"name": name, "message": "has left the room"}, to=room)
-    print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":  
     # app.run(host="0.0.0.0", port=8000, debug=True)
