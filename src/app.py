e11e4ae7369ffb6824977e2e72a113e39f86968b
niemiecjakub <kniemiec3@gmail.com>
2023-12-18 00:01:22 +0100

    readme

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 
 import config
-from flask import render_template
+from flask import render_template, redirect
 from champions import champion_read_all, champion_read_by_key
 from category import category_read_all, category_read_by_key, category_get_list
 from flask_cors import CORS, cross_origin
@@ -14,33 +14,8 @@ app.app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/")
 @cross_origin()
 def home():
-    category_list = category_get_list()
-    return render_template("home.html", category_list=category_list)
+    return redirect("/api/ui", code=302)
 
-#CHAMPION
-@app.route("/champion")
-@cross_origin()
-def champions():
-    champions = champion_read_all()
-    return render_template("champions.html", champions=champions)
-
-@app.route("/champion/<key>")
-@cross_origin()
-def champion(key):
-    champion = champion_read_by_key(key)
-    return render_template("champion.html", champion=champion)
-
-@app.route("/<category>")
-@cross_origin()
-def cateogies(category):
-    data = category_read_all(category)
-    return render_template("categories.html", category=category, data=data )
-
-@app.route("/<category>/<name>")
-@cross_origin()
-def category(category, name):
-    category = category_read_by_key(category, name)
-    return render_template("category.html", category=category, name=name)
 
 
 if __name__ == "__main__":  
